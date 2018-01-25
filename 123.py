ab8f681b9f5e90069721254f445bbc55360b1fe9
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-01-25 11:13:15 +0530

    bug removed: incorrect version of github3 used

--- 123.py
+++ 123.py
@@ -1,11 +1,53 @@
+# import github3
+# from BaseHTTPServer import HTTPServer
+# from BaseHTTPServer import BaseHTTPRequestHandler
+# import json
+#
+# class RestHTTPRequestHandler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.end_headers()
+#
+#         res = {}
+#         l = {}
+#         k = None
+#         li = []
+#         gh = github3.login("sanket386", "consultadd505")
+#         org = gh.organization("att")
+#         repos = list(org.iter_repos(type="public"))  # Or type="private"
+#         for r in repos:
+#             # print r.name
+#             k = r.name
+#             issuess = gh.iter_repo_issues("att", r.name)
+#
+#             for iss in issuess:
+#                 # print iss.title , iss.number
+#                 if iss.comments > 0:
+#                     # print iss.comments
+#                     l['issues'] = {'title': iss.title, 'number': iss.number, 'Comments': iss.comments}
+#                 else:
+#                     l['issues'] = {'title': iss.title, 'number': iss.number}
+#                 li.append(l['issues'])
+#             res[k] = li
+#             # print k
+#         r = json.dumps(res)
+#
+#         self.wfile.write(json.dumps({'data': r}))
+#         return
+#
+# httpd = HTTPServer(('0.0.0.0', 9001), RestHTTPRequestHandler)
+# while True:
+#     httpd.handle_request()
 import github3
-from BaseHTTPServer import HTTPServer
+# from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import json
 import os
-import socketserver
+import SocketServer
 
 PORT = 5001
+
+
 class RestHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -14,7 +56,7 @@ class RestHTTPRequestHandler(BaseHTTPRequestHandler):
         l = {}
         k = None
         li = []
-        gh = github3.login("yaswanthus93@gmail.com", "yaswanth12") #enter your github username and password
+        gh = github3.login("yaswanthus93@gmail.com", "yaswanth12")  # enter your github username and password
         org = gh.organization("att")
         # print org
         repos = list(org.iter_repos(type="public"))  # Or type="private"
@@ -25,20 +67,21 @@ class RestHTTPRequestHandler(BaseHTTPRequestHandler):
 
             for iss in issuess:
                 # print iss.title , iss.number
-                if iss.comments > 0:
-                    # print iss.comments
-                    l['issues'] = {'title': iss.title, 'number': iss.number, 'Comments': iss.comments}
-                else:
-                    l['issues'] = {'title': iss.title, 'number': iss.number}
-                li.append(l['issues'])
-            res[k] = li
-            # print k
+                if iss.state == "open":
+                    if iss.comments > 0:
+                        # print iss.comments
+                        l['issues'] = {'title': iss.title, 'number': iss.number, 'Comments': iss.comments}
+                    else:
+                        l['issues'] = {'title': iss.title, 'number': iss.number}
+                    li.append(l['issues'])
+                res[k] = li
+                # print k
         r = json.dumps(res)
 
         self.wfile.write(json.dumps({'data': r}))
         return
 
 
-httpd = socketserver.TCPServer(('',PORT), RestHTTPRequestHandler)
+httpd = SocketServer.TCPServer(('0.0.0.0', PORT), RestHTTPRequestHandler)
 while True:
     httpd.handle_request()
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python 123.py $PORT
+web: python 123.py
